a4ea07120ee27660342f975c6d559c1b96172d57
wcoltonweeks <wcoltonweeks@outlook.com>
2021-04-19 13:15:33 -0500

    Initial_commit_created_ui_layer

---
+++ FinalProject.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProject.UI.MVC.MvcApplication" Language="C#" %>
---
+++ FinalProject.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProject.UI.MVC.Startup))]
+namespace FinalProject.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
